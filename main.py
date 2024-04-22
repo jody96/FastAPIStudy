f27b6c5d65856c3995e38271086d166e44f1af42
daeyeolJo <daeyeol.kt@gmail.com>
2024-04-22 15:11:20 +0900

    query parameters

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 from fastapi import FastAPI
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id" : item_id}
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id" : "the current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description:":"This is an amazing item that has a long description"}
+        )
+    return item
     
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "leCNN all the images"}
-    
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        return item.update({"q": q})
+    if not short:
+        item.update(
+            {"description:":"This is an amazing item that has a long description"}
+        )
+    return item
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
